de6fddc58e33069666747f7fb8fb932a067d18f1
Anish <anishgowda43@gmail.com>
2025-09-29 00:32:22 +0530

    refactor: simplify configuration setup for upload handlers

--- src/pages/api/uploadthing.ts
+++ src/pages/api/uploadthing.ts
@@ -1,11 +1,21 @@
 import { createRouteHandler } from "uploadthing/server";
 import { ourFileRouter } from "../../server/uploadthing-router";
 
+let config: { token: any; callbackUrl?: any } = {
+  token: import.meta.env.UPLOADTHING_TOKEN,
+};
+
+if (import.meta.env.UPLOADTHING_CALLBACK_URL) {
+  console.log(
+    "Callback url found ===>",
+    import.meta.env.UPLOADTHING_CALLBACK_URL
+  );
+  config["callbackUrl"] = import.meta.env.UPLOADTHING_CALLBACK_URL;
+}
+
 const handlers = createRouteHandler({
   router: ourFileRouter,
-  config: {
-    token: import.meta.env.UPLOADTHING_TOKEN,
-  },
+  config,
 });
 
 export { handlers as GET, handlers as POST };
